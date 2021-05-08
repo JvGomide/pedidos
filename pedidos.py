f0dd532e2927fb912949e61623e0b93e4f6794bd
João Vitor Gomide <joaovitor_m4ster@hotmail.com>
2021-05-08 18:41:19 -0300

    Criação das funcoes
    Está quase tudo pronto, falta uma função e o teste contra antas

--- pedidos.py
+++ pedidos.py
@@ -1,20 +1,74 @@
-# Programa feito por Ariane Barros e João Vitor: 21003360
+# Programa feito por Ariane Paula Barros (21011817) e João Vitor Gomide Campos (21003360)
 
-end = False
 
-while not end:
-    Pedido = {}
+def leitura_pedidos (Pedidos):
+        Produto = {}
+
+        error = "ok"
     
-    cod_pedido = int(input("Digite o código do pedido: "))
+        cod_pedido = int(input("Digite o código do pedido: "))
+        if  cod_pedido != -1:
 
-    quantidade_pedida = int(input("Digite quantos produtos tem nesse pedido: "))
-    for i in range(quantidade_pedida):
-        num_produto = int(input("Digite o número do produto: "))
-        quantidade = int(input("Digite a quantidade pedida: "))
+            quantidade_produtos = int(input("Digite quantos produtos tem nesse pedido: "))
+        
+            for i in range(quantidade_produtos):
+                cod_produto = int(input("\nDigite o código do produto: "))   #verificar se ja foi usado
 
-    Pedido[cod_pedido] = [quantidade_pedida, num_produto, quantidade]
+                while not valida_produto(cod_produto, Tab_Preco):                    
+                    print("Produto não encontrado, tente novamente")
+                    cod_produto = int(input("\nDigite o código do produto: "))
+                    #quantidade_produtos += 1
+                    #break
+                                  
+                quantidade = int(input("Digite a quantidade pedida: "))
 
-    Pedido
+                Produto[cod_produto] = quantidade 
 
-    break
+            Pedidos[cod_pedido] = [quantidade_produtos, Produto]
+        else:
+            error = "Pedido inexiste, -1 mano"
+           
+        #return error
+            return -1
+        return 0
+
+def imprime_relatorio (Tab_Precos, Pedidos):
+        # essa função deverá imprimir o relatório pedido;
+    print("\nNo.Pedido  No.Produto  Nome    Quantidade Pedida    Preço Unitário   Valor")
+
+    for pedido_cod, produto_dic in Pedidos.items():     #perguntar se tem como fazer for com and
+        print(pedido_cod)  
+        #print(qnt_produtos)
+        print(produto_dic[0]) #qtd produtos
+        print(produto_dic[1])  #código e qtd dele
+
+        for produto_lista in Tab_Precos.items():        
+            if produto_lista[0] in produto_dic.items():
+                print(produto_lista[0])  #codigo do produto
+                print(produto_lista[1])   #nome
+                print(produto_lista[2])  #preco
+
+def valida_produto(CodigoProduto, Tab_Precos):
+    #– função que retorna True ou False se o código digitado no pedido existe ou não na Tabela de Preços;
+    
+    if CodigoProduto in Tab_Preco.keys():
+        return True
+            
+    return False
+
+acabou = False
+Pedidos = {}
+Tab_Preco = {1: ["Barra Diamante Negro 190g", 4.99], 2: ["Barra Lacta Laka 190g", 4.99], 3: ["Caixa de Bis 126g", 4.39], 4: ["Caixa de bombons NESTLE", 12.00],
+            5: ["Sorvete 1,5L", 22.99], 6: ["Kit Kat 41,5g", 1.99], 7: ["Trident", 2.99], 8: ["Twix", 2.97], 9: ["Snickers", 2.29],
+            10: ["Tubes Morango Azedinho 40g", 2.99], 11: ['Paçoquita 50 unidades', 32.13], 12: ['Sufflair', 4.41]}
+
+while not acabou:
+
+    if leitura_pedidos(Pedidos) == -1:
+        acabou = True
+        imprime_relatorio(Tab_Preco, Pedidos)
+    
+    #print(Pedidos)
+   
+
 print("Fim do programa \n" + "-------" * 25)
